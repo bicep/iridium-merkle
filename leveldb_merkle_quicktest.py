cacad63f0a7600078bd3c069c7fa699900c3f1ea
Xenonym <nganeloe@gmail.com>
2018-03-29 15:12:47 +0300

    Rewrite quick test output to be readable and refactor constants

--- leveldb_merkle_quicktest.py
+++ leveldb_merkle_quicktest.py
@@ -1,37 +1,45 @@
 #!/usr/bin/env python2
 
 import os
+import random
 
 import merkle
 import in_memory_merkle_tree
 import leveldb_merkle_tree
 
-LEAVES = [os.urandom(2048) for i in xrange(65536)]
+LEAF_SIZE = 2048
+NUM_LEAVES = 65536
+TEST_LEAF = random.randint(0, NUM_LEAVES)
+
+def encode_byte_list(l):
+    return [h.encode('hex') for h in l]
+
+LEAVES = [os.urandom(LEAF_SIZE) for i in xrange(NUM_LEAVES)]
 
 IMT = in_memory_merkle_tree.InMemoryMerkleTree(LEAVES)
-print IMT.get_root_hash().encode('hex')
+print "Root hash in InMemoryMerkleTree: " + IMT.get_root_hash().encode('hex')
 
 os.system("rm -rf ./merkle_db")
 LMT = leveldb_merkle_tree.LeveldbMerkleTree()
 for l in LEAVES:
     LMT.add_leaf(l)
 # LMT.extend(LEAVES)
-print LMT.get_root_hash().encode('hex')
+print "Root hash in LeveldbMerkleTree: " + LMT.get_root_hash().encode('hex')
 
 # check that root hashes are identical
-print IMT.get_root_hash() == LMT.get_root_hash()
+print "Are the two root hashes identical? %s: " % (IMT.get_root_hash() == LMT.get_root_hash())
 
 # check that consistency proof is working and identical
 MV = merkle.MerkleVerifier()
-const_proof = LMT.get_consistency_proof(20000)
-print [h.encode('hex') for h in const_proof]
-print MV.verify_tree_consistency(20000, 65536, IMT.get_root_hash(20000), LMT.get_root_hash(), const_proof)
+const_proof = LMT.get_consistency_proof(TEST_LEAF)
+print "Consistency proof for subtree at leaf %s:\n%s" % (TEST_LEAF, encode_byte_list(const_proof))
+print "Is consistency proof correct?: %s" % MV.verify_tree_consistency(TEST_LEAF, NUM_LEAVES, IMT.get_root_hash(TEST_LEAF), LMT.get_root_hash(), const_proof)
 
 # check that inclusion proof is working and identical
-leaf_hash = LMT.get_leaf(20000)
-incl_proof = LMT.get_inclusion_proof(20000)
-print [h.encode('hex') for h in incl_proof]
-print IMT.get_inclusion_proof(20000, 65536) == incl_proof
-print MV.verify_leaf_hash_inclusion(leaf_hash, 20000, incl_proof, LMT)
+leaf_hash = LMT.get_leaf(TEST_LEAF)
+incl_proof = LMT.get_inclusion_proof(TEST_LEAF)
+print "Inclusion proof for leaf %s:\n%s" % (TEST_LEAF, encode_byte_list(incl_proof))
+print "Are inclusion proofs from InMemoryMerkleTree and LeveldbMerkleTree identical?: %s" % (IMT.get_inclusion_proof(TEST_LEAF, NUM_LEAVES) == incl_proof)
+print "Is inclusion proof correct?: %s" % MV.verify_leaf_hash_inclusion(leaf_hash, TEST_LEAF, incl_proof, LMT)
 
 LMT.close()
